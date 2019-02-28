cb857f599646603560abf869f9c17545289a49f8
Sam Rayner <sam@samrayner.com>
2019-02-28 16:15:46 +0000

    Update to parse iOS crash files

--- symbolicate.py
+++ symbolicate.py
@@ -3,33 +3,28 @@ import ntpath
 from subprocess import Popen, PIPE, STDOUT
 import os
 
-
-def get_architecture(file_path_diag):
-    with open(file_path_diag) as f:
-        lines = f.readlines()
-        for line in lines:
-            if "Architecture:" in line:
-                words = line.split()
-                return words[1]
-
-    raise ValueError("didn't found architecture")
-
-
-def get_base_address(file_path_diag, module_name):
+def get_arch_and_base_address(file_path_diag, module_name):
     found_binary = False
     with open(file_path_diag) as f:
         lines = f.readlines()
         for line in lines:
             if found_binary and module_name in line and "0x" in line:
+                base_address = None
+                arch = None
                 words = line.split()
-                for word in words:
-                    if "0x" in word:
-                        print "found base address: " + word
-                        return word
+                for index, word in enumerate(words):
+                    if index > 0 and words[index - 1] == module_name:
+                        print "Found architecture: " + word
+                        arch = word
+                    if base_address == None and "0x" in word:
+                        print "Found base address: " + word
+                        base_address = word
+                    if arch and base_address:
+                        return (arch, base_address)
             elif "Binary Images:" in line:
                 found_binary = True
 
-    raise ValueError("didn't found binary image path of module" + module_name)
+    raise ValueError("Didn't find architecture or base address of module" + module_name)
 
 
 def get_method_address(line, module):
@@ -47,22 +42,19 @@ def read_crash(file_path_sym, file_path_diag, output_file_path):
     output_file = open(output_file_path, "w")
 
     module_name = ntpath.basename(file_path_sym)
-    print module_name
 
-    architecture = get_architecture(file_path_diag)
-
-    base_address = get_base_address(file_path_diag, module_name)
+    (arch, base_address) = get_arch_and_base_address(file_path_diag, module_name)
 
     with open(file_path_diag) as f:
         lines = f.readlines()
         for line in lines:
             is_translated = False
-            if module_name in line and "0x" in line and "???" in line:
+            if module_name in line and base_address in line:
 
                 method_address = get_method_address(line, module_name)
 
                 if method_address != "":
-                    cmd = 'atos -arch ' + architecture + " -o " + file_path_sym + " -l " + base_address + " " + method_address
+                    cmd = 'atos -arch ' + arch + " -o " + file_path_sym + " -l " + base_address + " " + method_address
                     print cmd
                     p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
                     atos_output = p.stdout.read()
@@ -77,12 +69,12 @@ def read_crash(file_path_sym, file_path_diag, output_file_path):
 arglen = len(sys.argv)
 
 if arglen < 3:
-    print 'missing input - usage symbolicate.py {xxx.dSYM/Contents/Resources/DWARF/xxx} {file.diag}'
+    print 'Missing input - symbolicate.py {xxx.dSYM/Contents/Resources/DWARF/xxx} {file.crash}'
 
 filename = ""
 
 if arglen == 3:
-    filename = "/tmp/translatedDiagnostics.log"
+    filename = "symbolicated.crash"
 else:
     filename = sys.argv[3]
 
@@ -93,4 +85,4 @@ except OSError:
 
 read_crash(sys.argv[1], sys.argv[2], filename)
 
-print 'output is written here: ' + filename
+print 'Symbolicated log to ' + filename
